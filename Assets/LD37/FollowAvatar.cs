997fe2d0d5baf877b25c606a8d96f8d312b0c5a5
Martin Vilcans <martin@librador.com>
2016-12-10 19:46:13 +0100

    Move camera a bit more lazily

--- Assets/LD37/FollowAvatar.cs
+++ Assets/LD37/FollowAvatar.cs
@@ -9,24 +9,34 @@ public class FollowAvatar : MonoBehaviour {
 
     private Vector3 delta;
     private Vector3 currentVelocity;
+
+    private Vector3 currentLookVelocity;
+    private Vector3 lookAtPosition;
     private Transform myTransform;
 
     void Awake() {
         myTransform = transform;
+        lookAtPosition = following.position;
         delta = following.position - myTransform.position;
     }
 
     void Update() {
         Vector3 wantedPosition;
+        Vector3 wantedLookAt;
 
         if(follow) {
             wantedPosition = following.position - delta;
+            wantedPosition.x *= .8f;  // make him a bit lazy about moving
+            wantedLookAt = following.position;
         }
         else {
             wantedPosition = myTransform.position;
+            wantedLookAt = myTransform.position + delta;
         }
         //Vector3 newPosition = Vector3.MoveTowards(myTransform.position, wantedPosition, moveSpeed * Time.unscaledDeltaTime);
         Vector3 newPosition = Vector3.SmoothDamp(myTransform.position, wantedPosition, ref currentVelocity, smoothTime);
+        lookAtPosition = Vector3.SmoothDamp(lookAtPosition, wantedLookAt, ref currentLookVelocity, smoothTime);
         myTransform.position = newPosition;
+        myTransform.LookAt(lookAtPosition);
     }
 }
