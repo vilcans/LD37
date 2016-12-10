50e20e6f59c611c1dde927d7e73166372ee01733
Martin Vilcans <martin@librador.com>
2016-12-10 14:46:08 +0100

    Tweak jumping physics

--- Assets/LD37/Movement.cs
+++ Assets/LD37/Movement.cs
@@ -6,20 +6,41 @@ public class Movement : MonoBehaviour {
 
     private const float movementForce = 5f;
     private const float stepForce = 2.5f;
-    private const float jumpForce = 1f;
+    private const float jumpForce = 3f;
     private Vector2 velocity;
 
+    private float timeSinceJumpRequest = 0;
+    private float timeSinceJump = 0;
+    private float timeSinceGround;
     private bool grounded;
 
     void FixedUpdate() {
+        if(grounded) {
+            timeSinceGround = 0;
+        }
+        else {
+            timeSinceGround += Time.deltaTime;
+        }
+        if(Input.GetButtonDown("Jump")) {
+            timeSinceJumpRequest = 0;
+        }
+        else {
+            timeSinceJumpRequest += Time.deltaTime;
+        }
+
         float accX = Input.GetAxis("Horizontal") * movementForce * Time.deltaTime;
         //Debug.LogFormat("accX={0}", accX);
         float accY = 0;
         if(grounded) {
             accY += Mathf.Abs(accX) * stepForce;
         }
-        if(Input.GetButton("Jump") && grounded) {
+
+        if(timeSinceJumpRequest < .20f && timeSinceGround < .04f && timeSinceJump >= .2f) {
+            timeSinceJump = 0;
             accY += jumpForce;
+        }
+        else {
+            timeSinceJump += Time.deltaTime;
         }
         GetComponent<Rigidbody2D>().AddForce(new Vector2(accX, accY));
         grounded = false;
