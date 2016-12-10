6f50fbcdd1cbdb2df78739c3be142b272ed44c9d
Martin Vilcans <martin@librador.com>
2016-12-10 19:01:16 +0100

    Follow avatar with camera

---
+++ Assets/LD37/FollowAvatar.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class FollowAvatar : MonoBehaviour {
+
+    public Transform following;
+    private const float smoothTime = 1f;
+
+    private Vector3 delta;
+    private Vector3 currentVelocity;
+    private Transform myTransform;
+
+    void Awake() {
+        myTransform = transform;
+        delta = following.position - myTransform.position;
+    }
+
+    void Update() {
+        Vector3 wantedPosition = following.position - delta;
+        //Vector3 newPosition = Vector3.MoveTowards(myTransform.position, wantedPosition, moveSpeed * Time.unscaledDeltaTime);
+        Vector3 newPosition = Vector3.SmoothDamp(myTransform.position, wantedPosition, ref currentVelocity, smoothTime);
+        myTransform.position = newPosition;
+    }
+}
